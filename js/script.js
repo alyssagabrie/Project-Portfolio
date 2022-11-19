1cef5de4411e5de44ca57d1576029879499abd5e
alyssagabrie <alyssaherrera92@gmail.com>
2022-11-18 21:38:30 -0500

    nav fix commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-const nav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-
-navToggle.addEventListener("click", () => {
-    
-    const visiblity = nav.getAttribute("data-visible");
-    if (visiblity === "false") {
-        nav.setAttribute("data-visible", true);
-    } else {
-        nav.setAttribute("data-visible", false);
-    }
-})
-
-
-// Tab Links 
-
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
